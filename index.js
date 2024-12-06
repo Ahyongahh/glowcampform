1bfe1df3b496a8110cfad12745f83d8dab36664b
Ahyongahh <143467078+Ahyongahh@users.noreply.github.com>
2024-12-06 23:56:34 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,62 @@
+import { initializeApp } from 'firebase/app'
+import {
+  getFirestore, collection, getDocs,
+  addDoc
+} from 'firebase/firestore'
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAHQCfMgzg8biego4qFGWKThseoi580L9s",
+  authDomain: "glowcampform-5161c.firebaseapp.com",
+  projectId: "glowcampform-5161c",
+  storageBucket: "glowcampform-5161c.firebasestorage.app",
+  messagingSenderId: "567662011896",
+  appId: "1:567662011896:web:6cb2de0ff0f63df5ae620d"
+};
+
+// init firebase
+initializeApp(firebaseConfig)
+
+// init services
+const db = getFirestore()
+
+// collection ref
+const colRef = collection(db, 'FormID')
+
+// Function to get the base64-encoded image from the canvas
+function getSignatureBase64() {
+  const canvas = document.getElementById('signatureCanvas');
+  return canvas.toDataURL(); // Converts the canvas content to a base64-encoded PNG string
+}
+
+// Adding docs to Firestore
+const addDetailsForm = document.querySelector('.add');
+addDetailsForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  // Create the formData object and capture the form input values
+  const formData = {
+    ChildName: document.getElementById('ChildName').value,
+    signature: getSignatureBase64(), // Get base64 string from canvas
+    ParentName: document.getElementById('ParentName').value,
+    RStoChild: document.getElementById('RStoChild').value,
+  };
+
+
+  // Add the formData to Firestore
+  addDoc(colRef, formData)
+    .then(() => {
+      console.log("Document successfully written!");
+
+      addDetailsForm.reset(); // Reset the form after successful submission
+      clearCanvas(); // Clear the canvas after submission
+
+      // Redirect to end.html
+    //  window.location.href = 'end.html';
+    })
+    .catch((err) => {
+      console.error("Error adding document: ", err);
+    });
+});
+
+
